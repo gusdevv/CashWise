6769872e33e5dc32d4ffc21c76e764efc1449d77
Gustavo Reyes <gustirt01@gmail.com>
2024-03-22 15:08:10 -0400

    Landing al 90%

--- js/landing.js
+++ js/landing.js
@@ -66,3 +66,19 @@ gsap.from(".team_img_wrapper img", {
   delay: 3,
   duration: 1,
 });
+
+// CARROUSEL ANIMATION
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
